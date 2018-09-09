9b794b9ecbaff4683cdb35264178ea6f6d9bbbb5
Tinsh <kilofox2000@gmail.com>
2018-09-09 21:41:11 +0800

    feat(*): isolate the visible part of an application inside a public directory

--- public/index.php
+++ public/index.php
@@ -53,7 +53,7 @@ error_reporting(E_ALL | E_STRICT);
  * @link http://kohanaframework.org/guide/using.configuration
  */
 // Set the full path to the docroot
-define('DOCROOT', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+define('DOCROOT', realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR);
 
 // Make the application relative to the docroot, for symlink'd index.php
 if (!is_dir($application) AND is_dir(DOCROOT . $application))
