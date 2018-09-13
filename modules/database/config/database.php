428ec223bd913a32713d748312a5551213e3b560
Tinsh <kilofox2000@gmail.com>
2018-09-13 19:33:05 +0800

    feat(Database): remove MySQL driver

--- modules/database/config/database.php
+++ modules/database/config/database.php
@@ -1,92 +1,62 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
-
-return array
-    (
-    'default' => array
-        (
-        'type' => 'MySQL',
-        'connection' => array(
+return [
+    'default' => [
+        'type' => 'PDO',
+        'connection' => [
             /**
-             * The following options are available for MySQL:
+             * The following options are available for PDO:
              *
-             * string   hostname     server hostname, or socket
-             * string   database     database name
-             * string   username     database username
-             * string   password     database password
-             * boolean  persistent   use persistent connections?
-             * array    variables    system variables as "key => value" pairs
+             * string   dsn         Data Source Name
+             * string   username    Database username
+             * string   password    Database password
+             * bool     persistent  Use persistent connections?
+             */
+            'dsn' => 'mysql:host=localhost;dbname=kohana',
+            'username' => 'root',
+            'password' => '',
+            'persistent' => false,
+        ],
+        /**
+         * The following extra options are available for PDO:
+         *
+         * string   identifier  Set the escaping identifier
+         */
+        'table_prefix' => '',
+        'charset' => 'utf8',
+        'caching' => false,
+    ],
+    'mysqli' => [
+        'type' => 'MySQLi',
+        'connection' => [
+            /**
+             * The following options are available for MySQLi:
+             *
+             * string   hostname    Server hostname, or socket
+             * string   database    Database name
+             * string   username    Database username
+             * string   password    Database password
+             * bool     persistent  Use persistent connections?
+             * array    ssl         SSL parameters as "key => value" pairs.
+             *                      Available keys: client_key_path, client_cert_path, ca_cert_path, ca_dir_path, cipher
+             * array    variables   System variables as "key => value" pairs
              *
              * Ports and sockets may be appended to the hostname.
              */
             'hostname' => 'localhost',
             'database' => 'kohana',
-            'username' => FALSE,
-            'password' => FALSE,
-            'persistent' => FALSE,
-        ),
-        'table_prefix' => '',
-        'charset' => 'utf8',
-        'caching' => FALSE,
-    ),
-    'alternate' => array(
-        'type' => 'PDO',
-        'connection' => array(
-            /**
-             * The following options are available for PDO:
-             *
-             * string   dsn         Data Source Name
-             * string   username    database username
-             * string   password    database password
-             * boolean  persistent  use persistent connections?
-             */
-            'dsn' => 'mysql:host=localhost;dbname=kohana',
-            'username' => 'root',
-            'password' => 'r00tdb',
-            'persistent' => FALSE,
-        ),
+            'username' => '',
+            'password' => '',
+            'persistent' => false,
+            'ssl' => false,
+        ],
         /**
          * The following extra options are available for PDO:
          *
-         * string   identifier  set the escaping identifier
+         * string   identifier  Set the escaping identifier
          */
         'table_prefix' => '',
         'charset' => 'utf8',
-        'caching' => FALSE,
-    ),
-    /**
-     * MySQLi driver config information
-     *
-     * The following options are available for MySQLi:
-     *
-     * string   hostname     server hostname, or socket
-     * string   database     database name
-     * string   username     database username
-     * string   password     database password
-     * boolean  persistent   use persistent connections?
-     * array    ssl          ssl parameters as "key => value" pairs.
-     *                       Available keys: client_key_path, client_cert_path, ca_cert_path, ca_dir_path, cipher
-     * array    variables    system variables as "key => value" pairs
-     *
-     * Ports and sockets may be appended to the hostname.
-     *
-     * MySQLi driver config example:
-     *
-     */
-// 	'alternate_mysqli' => array
-// 	(
-// 		'type'       => 'MySQLi',
-// 		'connection' => array(
-// 			'hostname'   => 'localhost',
-// 			'database'   => 'kohana',
-// 			'username'   => FALSE,
-// 			'password'   => FALSE,
-// 			'persistent' => FALSE,
-// 			'ssl'        => NULL,
-// 		),
-// 		'table_prefix' => '',
-// 		'charset'      => 'utf8',
-// 		'caching'      => FALSE,
-// 	),
-);
+        'caching' => false,
+    ],
+];
