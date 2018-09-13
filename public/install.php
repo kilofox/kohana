f1c6c26fc2b2ce7fe173f569b874514833aadc57
Tinsh <kilofox2000@gmail.com>
2018-09-13 17:58:19 +0800

    refactor(public): improve environment tests

--- public/install.php
+++ public/install.php
@@ -2,24 +2,15 @@
 // Sanity check, install should only be checked from index.php
 defined('SYSPATH') or exit('Install tests must be loaded from within index.php!');
 
-if (version_compare(PHP_VERSION, '5.3', '<')) {
-    // Clear out the cache to prevent errors. This typically happens on Windows/FastCGI.
-    clearstatcache();
-} else {
-    // Clearing the realpath() cache is only possible PHP 5.3+
-    clearstatcache(TRUE);
-}
+// Clear the realpath() cache
+clearstatcache(true);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
     <head>
-
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charset="utf-8">
         <title>Kohana Installation</title>
-
-        <style type="text/css">
+        <style>
             body { width: 42em; margin: 0 auto; font-family: sans-serif; background: #fff; font-size: 1em; }
             h1 { letter-spacing: -0.04em; }
             h1 + p { margin: 0 0 2em; color: #333; font-size: 90%; font-style: italic; }
@@ -35,66 +26,61 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
             #results.pass { background: #191; }
             #results.fail { background: #911; }
         </style>
-
     </head>
     <body>
-
         <h1>Environment Tests</h1>
-
         <p>
-            The following tests have been run to determine if <a href="http://kohanaframework.org/">Kohana</a> will work in your environment.
-            If any of the tests have failed, consult the <a href="http://kohanaframework.org/guide/about.install">documentation</a>
+            The following tests have been run to determine if <a href="http://kohana.top">Kohana</a> will work in your environment.
+            If any of the tests have failed, consult the <a href="https://kohana.top/guide/about.install">documentation</a>
             for more information on how to correct the problem.
         </p>
-
-        <?php $failed = FALSE ?>
-
+        <?php $failed = false ?>
         <table cellspacing="0">
             <tr>
                 <th>PHP Version</th>
-                <?php if (version_compare(PHP_VERSION, '5.3.3', '>=')): ?>
-                    <td class="pass"><?php echo PHP_VERSION ?></td>
-                <?php else: $failed = TRUE ?>
-                    <td class="fail">Kohana requires PHP 5.3.3 or newer, this version is <?php echo PHP_VERSION ?>.</td>
+                <?php if (PHP_VERSION_ID >= 50600): ?>
+                    <td class="pass"><?= PHP_VERSION ?></td>
+                <?php else: $failed = true ?>
+                    <td class="fail">Kohana requires PHP 5.6.0 or newer, this version is <?= PHP_VERSION ?>.</td>
                 <?php endif ?>
             </tr>
             <tr>
                 <th>System Directory</th>
-                <?php if (is_dir(SYSPATH) AND is_file(SYSPATH . 'classes/Kohana' . EXT)): ?>
-                    <td class="pass"><?php echo SYSPATH ?></td>
-                <?php else: $failed = TRUE ?>
+                <?php if (is_file(SYSPATH . 'classes/Kohana' . EXT)): ?>
+                    <td class="pass"><?= SYSPATH ?></td>
+                <?php else: $failed = true ?>
                     <td class="fail">The configured <code>system</code> directory does not exist or does not contain required files.</td>
                 <?php endif ?>
             </tr>
             <tr>
                 <th>Application Directory</th>
-                <?php if (is_dir(APPPATH) AND is_file(APPPATH . 'bootstrap' . EXT)): ?>
-                    <td class="pass"><?php echo APPPATH ?></td>
-                <?php else: $failed = TRUE ?>
+                <?php if (is_file(APPPATH . 'bootstrap' . EXT)): ?>
+                    <td class="pass"><?= APPPATH ?></td>
+                <?php else: $failed = true ?>
                     <td class="fail">The configured <code>application</code> directory does not exist or does not contain required files.</td>
                 <?php endif ?>
             </tr>
             <tr>
                 <th>Cache Directory</th>
-                <?php if (is_dir(APPPATH) AND is_dir(APPPATH . 'cache') AND is_writable(APPPATH . 'cache')): ?>
-                    <td class="pass"><?php echo APPPATH . 'cache/' ?></td>
-                <?php else: $failed = TRUE ?>
-                    <td class="fail">The <code><?php echo APPPATH . 'cache/' ?></code> directory is not writable.</td>
+                <?php if (is_dir(APPPATH . 'cache') && is_writable(APPPATH . 'cache')): ?>
+                    <td class="pass"><?= APPPATH . 'cache' . DIRECTORY_SEPARATOR ?></td>
+                <?php else: $failed = true ?>
+                    <td class="fail">The <code><?= APPPATH . 'cache' . DIRECTORY_SEPARATOR ?></code> directory is not writable.</td>
                 <?php endif ?>
             </tr>
             <tr>
                 <th>Logs Directory</th>
-                <?php if (is_dir(APPPATH) AND is_dir(APPPATH . 'logs') AND is_writable(APPPATH . 'logs')): ?>
-                    <td class="pass"><?php echo APPPATH . 'logs/' ?></td>
-                <?php else: $failed = TRUE ?>
-                    <td class="fail">The <code><?php echo APPPATH . 'logs/' ?></code> directory is not writable.</td>
+                <?php if (is_dir(APPPATH . 'logs') && is_writable(APPPATH . 'logs')): ?>
+                    <td class="pass"><?= APPPATH . 'logs' . DIRECTORY_SEPARATOR ?></td>
+                <?php else: $failed = true ?>
+                    <td class="fail">The <code><?= APPPATH . 'logs' . DIRECTORY_SEPARATOR ?></code> directory is not writable.</td>
                 <?php endif ?>
             </tr>
             <tr>
                 <th>PCRE UTF-8</th>
-                <?php if (!@preg_match('/^.$/u', 'ñ')): $failed = TRUE ?>
+                <?php if (!@preg_match('/^.$/u', 'ñ')): $failed = true ?>
                     <td class="fail"><a href="http://php.net/pcre">PCRE</a> has not been compiled with UTF-8 support.</td>
-                <?php elseif (!@preg_match('/^\pL$/u', 'ñ')): $failed = TRUE ?>
+                <?php elseif (!@preg_match('/^\pL$/u', 'ñ')): $failed = true ?>
                     <td class="fail"><a href="http://php.net/pcre">PCRE</a> has not been compiled with Unicode property support.</td>
                 <?php else: ?>
                     <td class="pass">Pass</td>
@@ -104,7 +90,7 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
                 <th>SPL Enabled</th>
                 <?php if (function_exists('spl_autoload_register')): ?>
                     <td class="pass">Pass</td>
-                <?php else: $failed = TRUE ?>
+                <?php else: $failed = true ?>
                     <td class="fail">PHP <a href="http://www.php.net/spl">SPL</a> is either not loaded or not compiled in.</td>
                 <?php endif ?>
             </tr>
@@ -112,7 +98,7 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
                 <th>Reflection Enabled</th>
                 <?php if (class_exists('ReflectionClass')): ?>
                     <td class="pass">Pass</td>
-                <?php else: $failed = TRUE ?>
+                <?php else: $failed = true ?>
                     <td class="fail">PHP <a href="http://www.php.net/reflection">reflection</a> is either not loaded or not compiled in.</td>
                 <?php endif ?>
             </tr>
@@ -120,7 +106,7 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
                 <th>Filters Enabled</th>
                 <?php if (function_exists('filter_list')): ?>
                     <td class="pass">Pass</td>
-                <?php else: $failed = TRUE ?>
+                <?php else: $failed = true ?>
                     <td class="fail">The <a href="http://www.php.net/filter">filter</a> extension is either not loaded or not compiled in.</td>
                 <?php endif ?>
             </tr>
@@ -128,14 +114,14 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
                 <th>Iconv Extension Loaded</th>
                 <?php if (extension_loaded('iconv')): ?>
                     <td class="pass">Pass</td>
-                <?php else: $failed = TRUE ?>
+                <?php else: $failed = true ?>
                     <td class="fail">The <a href="http://php.net/iconv">iconv</a> extension is not loaded.</td>
                 <?php endif ?>
             </tr>
             <?php if (extension_loaded('mbstring')): ?>
                 <tr>
                     <th>Mbstring Not Overloaded</th>
-                    <?php if (ini_get('mbstring.func_overload') & MB_OVERLOAD_STRING): $failed = TRUE ?>
+                    <?php if (ini_get('mbstring.func_overload') & MB_OVERLOAD_STRING): $failed = true ?>
                         <td class="fail">The <a href="http://php.net/mbstring">mbstring</a> extension is overloading PHP's native string functions.</td>
                     <?php else: ?>
                         <td class="pass">Pass</td>
@@ -144,7 +130,7 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
             <?php endif ?>
             <tr>
                 <th>Character Type (CTYPE) Extension</th>
-                <?php if (!function_exists('ctype_digit')): $failed = TRUE ?>
+                <?php if (!function_exists('ctype_digit')): $failed = true ?>
                     <td class="fail">The <a href="http://php.net/ctype">ctype</a> extension is not enabled.</td>
                 <?php else: ?>
                     <td class="pass">Pass</td>
@@ -152,27 +138,23 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
             </tr>
             <tr>
                 <th>URI Determination</th>
-                <?php if (isset($_SERVER['REQUEST_URI']) OR isset($_SERVER['PHP_SELF']) OR isset($_SERVER['PATH_INFO'])): ?>
+                <?php if (isset($_SERVER['REQUEST_URI']) or isset($_SERVER['PHP_SELF']) or isset($_SERVER['PATH_INFO'])): ?>
                     <td class="pass">Pass</td>
-                <?php else: $failed = TRUE ?>
+                <?php else: $failed = true ?>
                     <td class="fail">Neither <code>$_SERVER['REQUEST_URI']</code>, <code>$_SERVER['PHP_SELF']</code>, or <code>$_SERVER['PATH_INFO']</code> is available.</td>
                 <?php endif ?>
             </tr>
         </table>
-
-        <?php if ($failed === TRUE): ?>
+        <?php if ($failed === true): ?>
             <p id="results" class="fail">✘ Kohana may not work correctly with your environment.</p>
         <?php else: ?>
             <p id="results" class="pass">✔ Your environment passed all requirements.<br />
-                Remove or rename the <code>install<?php echo EXT ?></code> file now.</p>
+                Remove or rename the <code>install<?= EXT ?></code> file now.</p>
         <?php endif ?>
-
         <h1>Optional Tests</h1>
-
         <p>
             The following extensions are not required to run the Kohana core, but if enabled can provide access to additional classes.
         </p>
-
         <table cellspacing="0">
             <tr>
                 <th>PECL HTTP Enabled</th>
@@ -191,11 +173,19 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
                 <?php endif ?>
             </tr>
             <tr>
-                <th>mcrypt Enabled</th>
+                <th>OpenSSL Enabled</th>
+                <?php if (extension_loaded('openssl')): ?>
+                    <td class="pass">Pass</td>
+                <?php else: ?>
+                    <td class="fail">Kohana can use the <a href="http://php.net/openssl">OpenSSL</a> extension for the Encrypt class.</td>
+                <?php endif ?>
+            </tr>
+            <tr>
+                <th>Mcrypt Enabled</th>
                 <?php if (extension_loaded('mcrypt')): ?>
                     <td class="pass">Pass</td>
                 <?php else: ?>
-                    <td class="fail">Kohana requires <a href="http://php.net/mcrypt">mcrypt</a> for the Encrypt class.</td>
+                    <td class="fail">Kohana can use the <a href="http://php.net/mcrypt">Mcrypt</a> extension for the Encrypt class.</td>
                 <?php endif ?>
             </tr>
             <tr>
@@ -207,22 +197,13 @@ if (version_compare(PHP_VERSION, '5.3', '<')) {
                 <?php endif ?>
             </tr>
             <tr>
-                <th>MySQL Enabled</th>
-                <?php if (function_exists('mysql_connect')): ?>
-                    <td class="pass">Pass</td>
-                <?php else: ?>
-                    <td class="fail">Kohana can use the <a href="http://php.net/mysql">MySQL</a> extension to support MySQL databases.</td>
-                <?php endif ?>
-            </tr>
-            <tr>
                 <th>PDO Enabled</th>
                 <?php if (class_exists('PDO')): ?>
                     <td class="pass">Pass</td>
                 <?php else: ?>
-                    <td class="fail">Kohana can use <a href="http://php.net/pdo">PDO</a> to support additional databases.</td>
+                    <td class="fail">Kohana can use <a href="http://php.net/pdo">PDO</a> to support databases.</td>
                 <?php endif ?>
             </tr>
         </table>
-
     </body>
 </html>
