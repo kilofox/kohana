950f76099bd988c2d105150e7355621113acda0a
Loong <loong2460@gmail.com>
2025-04-08 17:51:59 +0800

    docs(*): hyphenate "up-to-date" correctly

--- system/classes/Kohana/Controller.php
+++ system/classes/Kohana/Controller.php
@@ -129,7 +129,7 @@ abstract class Kohana_Controller
     /**
      * Checks the browser cache to see the response needs to be returned,
      * execution will halt and a 304 Not Modified will be sent if the
-     * browser cache is up to date.
+     * browser cache is up-to-date.
      *
      *     $this->check_cache(sha1($content));
      *
