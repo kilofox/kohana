1aa933b9e3b871c9d93f4510677695e8d9827958
Loong <loong2460@gmail.com>
2025-04-22 09:56:19 +0800

    docs(*): correct possessives

--- modules/userguide/config/userguide.php
+++ modules/userguide/config/userguide.php
@@ -10,9 +10,9 @@ return [
     'show_comments' => Kohana::$environment === Kohana::PRODUCTION,
     // Leave this alone
     'modules' => [
-        // This should be the path to this modules userguide pages, without the 'guide/'. Ex: '/guide/modulename/' would be 'modulename'
+        // This should be the path to this module's userguide pages, without the 'guide/'. Ex: '/guide/modulename/' would be 'modulename'
         'userguide' => [
-            // Whether this modules userguide pages should be shown
+            // Whether this module's userguide pages should be shown
             'enabled' => true,
             // The name that should show up on the userguide index page
             'name' => 'Userguide',
