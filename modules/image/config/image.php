83066438e03e569c71dd39abae8146fa51269ecc
Loong <loong2460@gmail.com>
2025-06-25 18:13:34 +0800

    feat(Image): remove deprecated Image::$default_driver property

--- modules/image/config/image.php
+++ modules/image/config/image.php
@@ -2,7 +2,5 @@
 
 return [
     // Provide the default driver to use: e.g., a value of Imagick will use Image_Imagick as the driver class.
-    // If you set a value for this config key, then the Image::$default_driver static variable that was used
-    // to configure earlier versions will be ignored.
-    'default_driver' => null,
+    'default_driver' => 'GD',
 ];
