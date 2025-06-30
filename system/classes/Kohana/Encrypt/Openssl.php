5f8318de54abef0ef7e8897897cd5a5562162303
Loong <loong2460@gmail.com>
2025-06-30 14:46:45 +0800

    fix(Core): correct PHP version comparison for openssl_encrypt and openssl_decrypt

--- system/classes/Kohana/Encrypt/Openssl.php
+++ system/classes/Kohana/Encrypt/Openssl.php
@@ -112,7 +112,7 @@ class Kohana_Encrypt_Openssl
         }
 
         // Encrypt the data using the configured options and generated IV.
-        if (PHP_VERSION_ID >= 71000) {
+        if (PHP_VERSION_ID >= 70100) {
             $data = openssl_encrypt($data, $this->method, $this->key, $this->options, $iv, $this->tag, $this->aad, $this->tagLength);
         } else {
             $data = openssl_encrypt($data, $this->method, $this->key, $this->options, $iv);
@@ -153,7 +153,7 @@ class Kohana_Encrypt_Openssl
         $data = substr($data, $this->ivSize);
 
         // Return the decrypted data, trimming the \0 padding bytes from the end of the data.
-        if (PHP_VERSION_ID >= 71000) {
+        if (PHP_VERSION_ID >= 70100) {
             return rtrim(openssl_decrypt($data, $this->method, $this->key, $this->options, $iv, $this->tag, $this->aad, $this->tagLength), "\0");
         } else {
             return rtrim(openssl_decrypt($data, $this->method, $this->key, $this->options, $iv), "\0");
