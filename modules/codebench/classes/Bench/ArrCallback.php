edddb587881235f461494d1beafeba07b06625dc
Loong <loong2460@gmail.com>
2025-07-21 18:42:03 +0800

    test(Codebench): update callback string parsing benchmarks in ArrCallback

--- modules/codebench/classes/Bench/ArrCallback.php
+++ modules/codebench/classes/Bench/ArrCallback.php
@@ -7,34 +7,23 @@
  */
 class Bench_ArrCallback extends Codebench
 {
-    public $description = 'Parsing <em>command[param,param]</em> strings in <code>Arr::callback()</code>:
-		 http://github.com/shadowhand/kohana/commit/c3aaae849164bf92a486e29e736a265b350cb4da#L0R127';
+    public $description = 'Parsing <em>command(param,param)</em> strings in <code>Arr::callback()</code>.';
     public $loops = 10000;
     public $subjects = [
         // Valid callback strings
         'foo',
         'foo::bar',
-        'foo[apple,orange]',
-        'foo::bar[apple,orange]',
-        '[apple,orange]', // no command, only params
-        'foo[[apple],[orange]]', // params with brackets inside
+        'foo(apple,orange)',
+        'foo::bar(apple,orange)',
+        '(apple,orange)', // no command, only params
+        'foo((apple),(orange))', // params with brackets inside
         // Invalid callback strings
         'foo[apple,orange', // no closing bracket
     ];
 
-    public function bench_shadowhand($subject)
-    {
-        // The original regex we're trying to optimize
-        if (preg_match('/([^\[]*+)\[(.*)\]/', $subject, $match))
-            return $match;
-
-        return [];
-    }
-
     public function bench_geert_regex_1($subject)
     {
-        // Added ^ and $ around the whole pattern
-        if (preg_match('/^([^\[]*+)\[(.*)\]$/', $subject, $matches))
+        if (preg_match('/^([^(]*+)\((.*)\)$/', $subject, $matches))
             return $matches;
 
         return [];
@@ -44,7 +33,7 @@ class Bench_ArrCallback extends Codebench
     {
         // A rather experimental approach using \K which requires PCRE 7.2 ~ PHP 5.2.4
         // Note: $matches[0] = params, $matches[1] = command
-        if (preg_match('/^([^\[]*+)\[\K.*(?=\]$)/', $subject, $matches))
+        if (preg_match('/^([^(]*+)\(\K.*(?=\)$)/', $subject, $matches))
             return $matches;
 
         return [];
@@ -53,8 +42,8 @@ class Bench_ArrCallback extends Codebench
     public function bench_geert_str($subject)
     {
         // A native string function approach which beats all the regexes
-        if (strpos($subject, '[') !== false AND substr($subject, -1) === ']')
-            return explode('[', substr($subject, 0, -1), 2);
+        if (strpos($subject, '(') !== false AND substr($subject, -1) === ')')
+            return explode('(', substr($subject, 0, -1), 2);
 
         return [];
     }
