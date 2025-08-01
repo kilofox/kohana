20079ad28a516ce844ded786c6875eeb78ee89fe
Loong <loong2460@gmail.com>
2025-08-01 15:08:23 +0800

    refactor(*): remove MSIE-specific code for getting computed style

--- modules/userguide/views/userguide/examples/hello_world_error.php
+++ modules/userguide/views/userguide/examples/hello_world_error.php
@@ -29,17 +29,9 @@
 
         if (elem.style && elem.style['display']) {
             disp = elem.style['display'];
+        } else if (window.getComputedStyle) {
+            disp = document.defaultView.getComputedStyle(elem, null).getPropertyValue('display');
         }
-        else if (elem.currentStyle)
-            // For MSIE, naturally
-            {
-                disp = elem.currentStyle['display'];
-            }
-        else if (window.getComputedStyle)
-            // For most other browsers
-            {
-                disp = document.defaultView.getComputedStyle(elem, null).getPropertyValue('display');
-            }
 
         // Toggle the state of the "display" style
         elem.style.display = disp === 'block' ? 'none' : 'block';
